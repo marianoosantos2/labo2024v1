0084ba542f6978c0cc40aa310a36e8616813424f
marianoosantos2 <msantos2@mail.austral.edu.ar>
2024-05-03 19:46:29 +0100

    tp

--- src/workflow-01/521_CA_reparar_dataset_tp.r
+++ src/workflow-01/521_CA_reparar_dataset_tp.r
@@ -290,6 +290,46 @@ for (i in seq_len(nrow(resultados_filtrados))) {
 } 
 }
 #------------------------------------------------------------------------------
+# EXTRAPOLACION CON MES ANTERIOR Y ANTERIOR-ANTERIOR
+Corregir_extrapolar <- function(dataset) {
+  gc()
+
+
+# Calcular la proporción de ceros por 'foto_mes' y identificar combinaciones completas de ceros
+ratios_cero <- dataset[, lapply(.SD, function(x) sum(x == 0, na.rm = TRUE) / .N), by = foto_mes]
+resultados <- ratios_cero[, lapply(.SD, function(x) x == 1), by = foto_mes]
+resultados_long <- melt(resultados, id.vars = "foto_mes", variable.name = "Variable", value.name = "Todos_ceros")
+resultados_filtrados <- resultados_long[Todos_ceros == TRUE, .(Variable, foto_mes)]
+resultados_filtrados[, Variable := gsub("Ratio_", "", Variable)]
+
+for (i in seq_len(nrow(resultados_filtrados))) {
+  var_var <- resultados_filtrados$Variable[i]
+  
+  var_mes  <- resultados_filtrados$foto_mes[i]
+  var_mes_ant <- var_mes - 1
+  var_mes_ant_ant <- var_mes - 2
+  
+  clientes_mes <- dataset[foto_mes == var_mes, .(numero_de_cliente)]
+  clientes_mes_ant <- dataset[foto_mes == var_mes_ant, .(numero_de_cliente)]
+  clientes_mes_ant_ant <- dataset[foto_mes == var_mes_ant_ant, .(numero_de_cliente)]
+  clientes_com <- intersect(intersect(clientes_mes$numero_de_cliente, clientes_mes_ant$numero_de_cliente), clientes_mes_ant_ant$numero_de_cliente)
+  
+  valor_cliente_mes_ant <- dataset[foto_mes == var_mes_ant & numero_de_cliente %in% clientes_com, .(valor = get(var_var))]
+  valor_cliente_mes_ant_ant <- dataset[foto_mes == var_mes_ant_ant & numero_de_cliente %in% clientes_com, .(valor = get(var_var))]
+  print(valor_cliente_mes_ant)
+  
+  new_val <- 2 * valor_cliente_mes_ant$valor -  valor_cliente_mes_ant_ant$valor
+  dataset[foto_mes == var_mes & numero_de_cliente %in% clientes_com, (var_var) := new_val]
+  
+} 
+}
+
+
+
+
+#------------------------------------------------------------------------------
+
+
 #------------------------------------------------------------------------------
 # Aqui empieza el programa
 OUTPUT$PARAM <- PARAM
